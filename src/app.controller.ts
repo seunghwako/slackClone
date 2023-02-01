d8ae308afe1b2a33a2691ffffe6f02a2dbf03629
seunghwako <seunghwa333@gmail.com>
2023-02-01 15:23:05 +0900

    Feat: 환경변수 사용을 위한 ConfigModule 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// Decorator만 붙여 놓으면 나머지는 Nest에서 알아서 다 해줌 -> IOC(Inversion Of Control) : 제어의 역전
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -10,3 +11,5 @@ export class AppController {
     return this.appService.getHello();
   }
 }
+
+// Nest는 module을 직접 구성해야 한다는 점에서 스프링보다 IOC가 약하다.
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
+import { ConfigService } from '@nestjs/config';
 import { Injectable } from '@nestjs/common';
 
+// service의 역할 -> 비즈니스 로직의 분리
+// 요청, 응답 없이 순수하게 비즈니스 로직만 작성
+// 테스트 할 때 편리
+// 재사용성 높아짐
 @Injectable()
 export class AppService {
+  constructor(private configService: ConfigService) {}
   getHello(): string {
-    return 'Hello World!';
+    return this.configService.get('PORT');
   }
 }
