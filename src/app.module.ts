4e3ff8b3f0716e8cf46040a60a3a0c0237e46619
seunghwako <seunghwa333@gmail.com>
2023-02-01 16:04:08 +0900

    Feat: Logging을 위한 미들웨어 적용용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerMiddleware } from './middlewares/logger.middleware';
 
+// .env 파일을 외부(클라우드 or 외부 저장소 ,,,,,)에서 받아오는 경우
+// const getEnv = async () => {
+//   const response = await axios.get('/env요청')
+//   return response.data
+// }
+
+// 위 경우에는 ConfigModule.forRoot에 option으로 load: [getEnv]와 같이 추가해줘야 함
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middlewares/logger.middleware.ts
@@ -0,0 +1,28 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private logger = new Logger('HTTP'); // HTTP(context)의 역할 -> HTTP 관련된 요청에서만 logger가 실행 됨 , express의 debug 라이브러리와 같은 역할
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { ip, method, originalUrl } = request;
+    const userAgent = request.get('user-agent') || ''; // header에서 가져옴
+
+    // 응답이 끝났을 때
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const contentLength = response.get('content-length');
+      this.logger.log(
+        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
+      );
+    });
+
+    next();
+  }
+}
+
+// middleware 자체는 router 즉, controller보다 먼저 실행됨
+// line 9-10 -> request에 대한 기록을 하고 나서
+// line 21 -> router로 이동
+// line 13-19 -> router가 finish된 후 실행
